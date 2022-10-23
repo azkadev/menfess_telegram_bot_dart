ee0a6bda8cded809db66531917b9030c365cc7bc
Your Name <you@example.com>
2022-10-23 15:12:19 +0700

    first commit

--- bin/menfess_telegram_bot_dart.dart
+++ bin/menfess_telegram_bot_dart.dart
@@ -1,18 +1,14 @@
 // ignore_for_file: unnecessary_brace_in_string_interps, non_constant_identifier_names, unused_local_variable, avoid_init_to_null, depend_on_referenced_packages
-
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:telegram_client/telegram_client.dart';
 import 'package:alfred/alfred.dart';
 import 'package:galaxeus_lib/galaxeus_lib.dart';
 import 'package:path/path.dart' as p;
-
 void main(List<String> arguments) async {
   String api_id = "";
   String api_hash = "";
   String username_channel = "@username_channel";
-
   Directory current_dir = Directory.current;
   String db_bot_api = p.join(current_dir.path, "bot_api");
   Directory dir_bot_api = Directory(db_bot_api);
@@ -21,7 +17,6 @@ void main(List<String> arguments) async {
   }
   int port = int.parse(Platform.environment["PORT"] ?? "8080");
   String host = Platform.environment["HOST"] ?? "0.0.0.0";
-
   String token_bot = "";
   TelegramBotApiServer telegramBotApiServer = TelegramBotApiServer();
   telegramBotApiServer.run(
@@ -36,15 +31,12 @@ void main(List<String> arguments) async {
   TelegramBotApi tg = TelegramBotApi(token_bot, clientOption: {
     "api": "http://0.0.0.0:9000/",
   });
-
   tg.request("setWebhook", parameters: {"url": "http://${host}:${port}"});
   Alfred app = Alfred();
   EventEmitter eventEmitter = EventEmitter();
-
   eventEmitter.on("update", null, (ev, context) async {
     if (ev.eventData is Map) {
       Map update = (ev.eventData as Map);
-
       if (update["channel_post"] is Map) {
         Map msg = (update["channel_post"] as Map);
         Map chat = msg["chat"];
@@ -70,7 +62,6 @@ void main(List<String> arguments) async {
         String? text = msg["text"];
         String? caption = msg["caption"];
         late String? msg_caption = null;
-
         if (text != null) {
           msg_caption = text;
           if (RegExp(r"/start", caseSensitive: false).hasMatch(text)) {
@@ -81,7 +72,6 @@ void main(List<String> arguments) async {
         if (caption != null) {
           msg_caption = caption;
         }
-
         if (msg_caption != null) {
           if (chat_type == "private") {
             if (RegExp(r"#(ask|curhat)", caseSensitive: false).hasMatch(msg_caption)) {
@@ -101,7 +91,6 @@ void main(List<String> arguments) async {
       }
     }
   });
-
   app.all("/", (req, res) async {
     if (req.method.toLowerCase() != "post") {
       return res.json({"@type": "ok", "message": "server run normal"});
@@ -111,8 +100,6 @@ void main(List<String> arguments) async {
       return res.json({"@type": "ok", "message": "server run normal"});
     }
   });
-
   await app.listen(port, host);
-
   print("Server run on ${app.server!.address.address}}");
 }
