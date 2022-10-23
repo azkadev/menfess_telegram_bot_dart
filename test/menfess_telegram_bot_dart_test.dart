eda1a9ec5d529b799e16b3aaad8cfd92fcb55ee9
Your Name <you@example.com>
2022-10-23 15:09:05 +0700

    first commit

---
+++ test/menfess_telegram_bot_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:menfess_telegram_bot_dart/menfess_telegram_bot_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
